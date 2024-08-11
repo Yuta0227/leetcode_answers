060930da9bc538ea8d2582af0792f51114ade2a8
Yuta Honjo <yuta0227.msc28@gmail.com>
2024-08-11 22:19:02 +0900

    created code for problem 141

---
+++ 141_linked_list_cycle.go
@@ -0,0 +1,20 @@
+package main
+
+//  Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	visited := make(map[*ListNode]bool)
+	current := head
+	for current != nil {
+		if visited[current] {
+			return true
+		}
+		visited[current] = true
+		current = current.Next
+	}
+	return false
+}
